9de76f83a0b025dbcf38af3d51af9f92500574ae
alkenzy <43424568+alkenzy@users.noreply.github.com>
2021-06-21 05:44:43 +0200

    tester script

--- CNN_tester.py
+++ CNN_tester.py
@@ -122,6 +122,17 @@ num_classes = 2
 
 # get the model using our helper function
 model = torchvision.models.detection.maskrcnn_resnet50_fpn() # we do not specify pretrained=True, i.e. do not load default weights
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+hidden_layer = 256
+    # and replace the mask predictor with a new one
+model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
 model.load_state_dict(torch.load("my_model.pth.tar")['state_dict'])
 model.to(device)
 # construct an optimizer
