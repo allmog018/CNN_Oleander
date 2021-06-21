114fd0e2a5955968a5730ae55ecbbc39cfb7e648
alkenzy <43424568+alkenzy@users.noreply.github.com>
2021-06-21 20:03:55 +0200

    fasterrcnn model added

--- FasterRCNN_maker.py
+++ FasterRCNN_maker.py
@@ -95,15 +95,7 @@ def get_instance_segmentation_model(num_classes):
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    # now get the number of input features for the mask classifier
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
-    # and replace the mask predictor with a new one
-    model.roi_heads.mask_predictor = FastRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
-
+    
     return model
     
 # import helper functions
--- FasterRCNN_trainer.py
+++ FasterRCNN_trainer.py
@@ -126,14 +126,6 @@ model = torchvision.models.detection.fasterrcnn_resnet50_fpn() # we do not speci
 in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
 model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    # now get the number of input features for the mask classifier
-in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-hidden_layer = 256
-    # and replace the mask predictor with a new one
-model.roi_heads.mask_predictor = FastRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
 model.load_state_dict(torch.load("my_fasterrcnn_model.pth.tar")['state_dict'])
 model.to(device)
 
